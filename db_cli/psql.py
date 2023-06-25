df575697f35909df871134e3f21b8c12f71dc6af
Freedom Loisa <kitakayaloisa@gmail.com>
2023-06-25 12:17:03 +0300

    class method to create record

--- db_cli/psql.py
+++ db_cli/psql.py
@@ -1,4 +1,5 @@
 import click
+from datetime import datetime
 import psycopg2  # type: ignore
 from configparser import ConfigParser
 
@@ -75,7 +76,7 @@ class PostgresConnect:
             click.echo(
                 click.style(
                     "The following tables have been created:\n",
-                    fg="blue",
+                    fg="green",
                     bold=True,
                     underline=True,
                 )
@@ -84,7 +85,7 @@ class PostgresConnect:
             count = 1
 
             for table in tables:
-                click.echo(click.style(f"{count}. {table}\n", fg="blue", bold=True))
+                click.echo(click.style(f"{count}. {table}\n", fg="green", bold=True))
 
                 count += 1
 
@@ -115,8 +116,8 @@ class PostgresConnect:
 
             click.echo(
                 click.style(
-                    "All available tables:\n",
-                    fg="blue",
+                    "List of all the tables in the database:\n",
+                    fg="cyan",
                     bold=True,
                     underline=True,
                 )
@@ -125,9 +126,48 @@ class PostgresConnect:
             count = 1
 
             for table in tables:
-                click.echo(click.style(f"{count}. {table}\n", fg="blue", bold=True))
+                click.echo(click.style(f"{count}. {table}\n", fg="cyan", bold=True))
 
                 count += 1
+
+            cur.close()
+
+        except Exception as error:
+            click.echo(click.style(f"{error}", fg="red", bold=True))
+
+        finally:
+            if conn is not None:
+                conn.close()
+
+        return
+
+    def create_record(
+        self,
+        animal: str,
+        morning_production: int,
+        afternoon_production: int,
+        evening_production: int,
+        production_unit: str,
+        production_date: datetime,
+    ):
+        conn = None
+
+        try:
+            conn = psycopg2.connect(**self.db)
+
+            cur = conn.cursor()
+
+            cur.execute(
+                f"INSERT INTO milk_production(animal, morning_production, afternoon_production, evening_production, production_unit, production_date), ({animal}, {morning_production}, { afternoon_production}, {evening_production}, {production_unit}, {production_date})"
+            )
+
+            conn.commit()
+
+            click.echo(
+                click.style(
+                    "Record has been created successfully.", fg="green", bold=True
+                )
+            )
 
             cur.close()
 
@@ -165,13 +205,21 @@ def create_tables(path: str):
 
 
 @click.command()
-def check_tables():
+def view_tables():
     my_db.view_tables()
 
 
+@click.command()
+def create_record():
+    pass
+
+
 cli.add_command(check_connection)
+
 cli.add_command(create_tables)
-cli.add_command(check_tables)
+cli.add_command(view_tables)
+
+cli.add_command(create_record)
 
 
 if __name__ == "__main__":
